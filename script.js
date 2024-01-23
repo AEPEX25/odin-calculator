a21f599b4133cc6a312dcfc57d516ae549b7ef21
Aepex25 <angelestevezsalinas@gmail.com>
2024-01-23 10:23:29 -0500

    complete js code reset

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 "use strict";
 
-const display = document.querySelector(".on-display");
+let operator = "";
+let previousValue = "";
+let currentValue = "";
 
-const add = (numOne, numTwo) => numOne + numTwo;
-const subtract = (numOne, numTwo) => numOne - numTwo;
-const multiply = (numOne, numTwo) => numOne * numTwo;
-const divide = (numOne, numTwo) => numOne / numTwo;
-
-const operate = function (numOne, numTwo) {
-  return divide(numOne, numTwo);
-};
-
-function onDisplay(button) {
-  let btnValues = button.value;
-  display.textContent = `${btnValues}`;
-  return btnValues;
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const display = document.querySelector(".on-display");
+});
