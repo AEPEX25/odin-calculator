86d531254240e7cb32cea58fe055b8a485364fa5
Aepex25 <angelestevezsalinas@gmail.com>
2024-01-23 11:25:44 -0500

    numbers and operators on display

--- script.js
+++ script.js
@@ -5,5 +5,36 @@ let previousValue = "";
 let currentValue = "";
 
 document.addEventListener("DOMContentLoaded", function () {
-  const display = document.querySelector(".on-display");
+  const clear = document.querySelector(".all-clear");
+  const equal = document.querySelector(".btn-equal");
+  const decimal = document.querySelector(".btn-decimal");
+
+  let displayCurrentValue = document.querySelector(".current-value");
+  let displayPreviousValue = document.querySelector(".previous-value");
+  let operants = document.querySelectorAll(".btn-operant");
+  let numbers = document.querySelectorAll(".btn-number");
+
+  numbers.forEach((number) =>
+    number.addEventListener("click", function (e) {
+      handleNumber(e.target.textContent);
+      displayCurrentValue.textContent = currentValue;
+    })
+  );
+
+  operants.forEach((operant) =>
+    operant.addEventListener("click", function (e) {
+      handleOperant(e.target.textContent);
+      displayCurrentValue.textContent = operator;
+      displayPreviousValue.textContent = currentValue + operator;
+      currentValue = "";
+    })
+  );
 });
+
+const handleNumber = function (num) {
+  currentValue += num;
+};
+
+const handleOperant = function (op) {
+  operator = op;
+};
